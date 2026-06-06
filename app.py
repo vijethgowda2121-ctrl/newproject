b1ab3187d623bbca5bf790bce48bee4e1007e0e4
vijethgowda2121-ctrl <vijethgowda2121@gmail.com>
2026-06-06 13:45:05 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="WiFi Hotspot Analyzer",
+    page_icon="📶",
+    layout="wide"
+)
+
+st.title(
+    "📶 Public Wi-Fi Hotspot Usage Analyzer"
+)
+
+st.markdown("""
+### Smart City Analytics Dashboard
+
+Analyze hotspot usage,
+crowd density,
+peak traffic periods,
+and user behavior.
+""")
+
+st.info(
+"Select a page from the sidebar."
+)
